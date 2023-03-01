2da9674260a7ca24fdb467dcae57ac4d518cf918
Rahmat Sulistio <rr7465457@gmail.com>
2023-03-01 23:12:34 +0700

    add timer modal

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,86 @@
-import { CSSProperties } from 'react'
+import { ChangeEvent, CSSProperties, KeyboardEvent, useRef, useState } from 'react'
+
+interface TimerForm {
+  minutes: string
+  seconds: string
+}
 
 function App(): JSX.Element {
+  const modalTriggerRef = useRef<HTMLInputElement>(null)
+  const [timerForm, setTimerForm] = useState<TimerForm>({ minutes: '', seconds: '' })
+
+  const toggleModal = (): void => {
+    modalTriggerRef.current?.click()
+  }
+
+  const handleChangeForm = (e: ChangeEvent<HTMLInputElement>): void => {
+    setTimerForm((_timerForm) => ({
+      ..._timerForm,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleSaveTimer = (): void => {
+    console.log({ timerForm })
+    toggleModal()
+  }
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>): void => {
+    if (['e', '-', ',', '.'].includes(e.key)) {
+      e.preventDefault()
+    }
+  }
+
   return (
     <div className="container bg-slate-900 h-screen grid place-items-center">
+      <input type="checkbox" className="modal-toggle" ref={modalTriggerRef} />
+      <div className="modal">
+        <div className="modal-box flex w-fit max-h-fit gap-2">
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text-alt">Minutes</span>
+            </label>
+            <input
+              type="number"
+              placeholder="0"
+              className="input input-bordered input-sm w-28"
+              min={0}
+              name="minutes"
+              value={timerForm.minutes}
+              onChange={handleChangeForm}
+              onKeyDown={onKeyDown}
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text-alt">Seconds</span>
+            </label>
+            <input
+              type="number"
+              placeholder="0"
+              className="input input-bordered input-sm w-28"
+              min={0}
+              name="seconds"
+              value={timerForm.seconds}
+              onChange={handleChangeForm}
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="label-text-alt">&nbsp;</span>
+            </label>
+            <button className="btn btn-sm btn-success" onClick={handleSaveTimer}>
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div className="flex items-center gap-5">
         <div className="flex flex-col justify-between gap-2">
-          <button className="btn btn-sm btn-info">Set Timer</button>
+          <button className="btn btn-sm btn-info" onClick={toggleModal}>
+            Set Timer
+          </button>
           <button className="btn btn-sm btn-success">Play</button>
           <button className="btn btn-sm btn-warning">Pause</button>
           <button className="btn btn-sm btn-error">Reset</button>
