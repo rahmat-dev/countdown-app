d535bd82df505f67b894519690cb212b73b1592e
Rahmat Sulistio <rr7465457@gmail.com>
2023-03-01 23:10:16 +0700

    slicing countdown timer app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,29 @@
+import { CSSProperties } from 'react'
+
 function App(): JSX.Element {
   return (
-    <div>
-      <h1 className="text-red-500 text-4xl">Hello, Electron Vite!</h1>
-      <button className="btn">Click Me</button>
-      <button className="btn btn-primary">Click Me</button>
+    <div className="container bg-slate-900 h-screen grid place-items-center">
+      <div className="flex items-center gap-5">
+        <div className="flex flex-col justify-between gap-2">
+          <button className="btn btn-sm btn-info">Set Timer</button>
+          <button className="btn btn-sm btn-success">Play</button>
+          <button className="btn btn-sm btn-warning">Pause</button>
+          <button className="btn btn-sm btn-error">Reset</button>
+        </div>
+        <div className="grid grid-flow-col text-center auto-cols-max items-center">
+          <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+            <span className="countdown font-mono text-8xl">
+              <span style={{ '--value': 15 } as CSSProperties}></span>
+            </span>
+          </div>
+          <span className="font-mono text-8xl">:</span>
+          <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+            <span className="countdown font-mono text-8xl">
+              <span style={{ '--value': 15 } as CSSProperties}></span>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
