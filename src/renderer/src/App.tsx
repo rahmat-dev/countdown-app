b15ebf39c3f887b93f68f2466e4d290729ed2174
Rahmat Sulistio <rr7465457@gmail.com>
2023-03-01 23:09:43 +0700

    init, configure window, setup daisyui

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1 className="text-red-500 text-4xl">Hello, Electron Vite!</h1>
+      <button className="btn">Click Me</button>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  )
+}
+
+export default App
